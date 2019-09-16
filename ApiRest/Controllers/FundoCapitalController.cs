96834d92910df804fde0cdc0c048dceaa12311ff
Leozera <leobhal13@gmail.com>
2019-09-16 17:38:33 -0300

    Adicionando nosso contrato

--- ApiRest/Controllers/FundoCapitalController.cs
+++ ApiRest/Controllers/FundoCapitalController.cs
@@ -3,43 +3,66 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ApiRest.Models;
+using ApiRest.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ApiRest.Controllers
 {
     public class FundoCapitalController : Controller
     {
+
+        private readonly IFundocapitalRepository _repository;
+
+
         [HttpGet("v1/fundocapital")]
         public IActionResult ListarFundos()
         {
-            return Ok(
-                new List<FundoCapital>
-                {
-                    new FundoCapital
-                    {
-                        Nome = "Viagem de Ferias",
-                        ValorAtual=200,
-                        ValorNecessario=5000,
-                        DataResgate = new DateTime(2019,12,1)
-                    },
-                      new FundoCapital
-                    {
-                        Nome = "Reserva de emergencia",
-                        ValorAtual=400,
-                        ValorNecessario=10000,
-                    }
-                });
+            return Ok(_repository.ListarFundos());
         }
 
         [HttpPost("v1/fundocapital")]
         public IActionResult AdicionarFundo([FromBody]FundoCapital fundo)
         {
+            _repository.Adicionar(fundo);
             return Ok();
         }
 
         [HttpPut("v1/fundocapital/{id}")]
         public IActionResult AlterarFundo(Guid id, [FromBody] FundoCapital fundo)
         {
+            var fundoAntigo = _repository.ObterPorId(id);
+            if (fundoAntigo == null)
+            {
+                return NotFound();
+            }
+            fundoAntigo.Nome = fundo.Nome;
+            fundoAntigo.ValorAtual = fundo.ValorAtual;
+            fundoAntigo.ValorNecessario = fundo.ValorNecessario;
+            fundoAntigo.DataResgate = fundo.DataResgate;
+            _repository.Alterar(fundoAntigo);
+            return Ok();
+        }
+
+        [HttpGet("v1/fundocapital/{id}")]
+        public IActionResult ObterFundo(Guid id)
+        {
+            var fundoAntigo = _repository.ObterPorId(id);
+            if (fundoAntigo == null)
+            {
+                return NotFound();
+            }        
+            return Ok(fundoAntigo);
+        }
+
+        [HttpDelete("v1/fundocapital/{id}")]
+        public IActionResult RemoverFundo(Guid id)
+        {
+            var fundo = _repository.ObterPorId(id);
+            if (fundo == null)
+            {
+                return NotFound();
+            }
+            _repository.RemoverFundo(fundo);
             return Ok();
         }
     }
---
+++ ApiRest/Repositories/IFundocapitalRepository.cs
@@ -0,0 +1,18 @@
+﻿using ApiRest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiRest.Repositories
+{
+    public interface IFundocapitalRepository
+    {
+        void Adicionar(FundoCapital fundo);
+        void Alterar(FundoCapital fundo);
+        IEnumerable<FundoCapital> ListarFundos();
+        FundoCapital ObterPorId(Guid Id);
+        void RemoverFundo(FundoCapital fundo);
+
+    }
+}
