ea577e486d84ff890482bebfd6cd0e9af06e65ad
Leozera <leobhal13@gmail.com>
2019-09-16 17:01:51 -0300

    Api inicializada, adicionado controllers, models e entidades

---
+++ ApiRest/Controllers/FundoCapitalController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiRest.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiRest.Controllers
+{
+    public class FundoCapitalController : Controller
+    {
+        [HttpGet("v1/fundocapital")]
+        public IActionResult ListarFundos()
+        {
+            return Ok(
+                new List<FundoCapital>
+                {
+                    new FundoCapital
+                    {
+                        Nome = "Viagem de Ferias",
+                        ValorAtual=200,
+                        ValorNecessario=5000,
+                        DataResgate = new DateTime(2019,12,1)
+                    },
+                      new FundoCapital
+                    {
+                        Nome = "Reserva de emergencia",
+                        ValorAtual=400,
+                        ValorNecessario=10000,
+                    }
+                });
+        }
+
+        [HttpPost("v1/fundocapital")]
+        public IActionResult AdicionarFundo([FromBody]FundoCapital fundo)
+        {
+            return Ok();
+        }
+
+        [HttpPut("v1/fundocapital/{id}")]
+        public IActionResult AlterarFundo(Guid id, [FromBody] FundoCapital fundo)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ ApiRest/Models/FundoCapital.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiRest.Models
+{
+    public class FundoCapital
+    {
+        public FundoCapital()
+        {
+            Id = Guid.NewGuid();
+        }
+        public Guid Id { get; }
+        public string Nome { get; set; }
+        public decimal ValorNecessario { get; set; }
+        public decimal ValorAtual { get; set; }
+        public DateTime? DataResgate { get; set; }
+    }
+}
